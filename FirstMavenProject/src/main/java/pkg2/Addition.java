c968dd6a63aba9bf650bbcb4692a68bbfb622142
Arathy <Arathy@DSNPVT>
2023-06-25 14:27:46 +0530

    Addition class

---
+++ FirstMavenProject/src/main/java/pkg2/Addition.java
@@ -0,0 +1,18 @@
+package pkg2;
+
+public class Addition {
+int a=15;
+int b=10;
+
+
+	public static void main(String[] args) {
+		
+		Addition obj=new Addition();
+		System.out.println(obj.sum());
+		
+	}
+	public  int sum() {
+		int result=a+b;
+		return a+b;
+	}
+}
